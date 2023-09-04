fba90650e2ba24c7173ebcb44310ff368ddd9f56
khakimdon <almamedovkh@mail.ru>
2023-09-04 20:53:38 +0500

    first commit

---
+++ src/components/Header.vue
@@ -0,0 +1,161 @@
+<script setup>
+
+</script>
+<script>
+import store from '../store';
+export default {
+    data() {
+        return {
+            env: store.state.lang,
+            text: store.state.languages,
+            ru: false,
+            en: true,
+            uz: false,
+            burger: false,
+        }
+    },
+    methods: {
+        BurgerFunc() {
+            this.burger = !this.burger
+        },
+        // changeLanguage(){
+        //     if(event.target.dataset.language == "uz"){
+        //         store.state.lang = 1
+        //         this.ru = false
+        //         this.en = false
+        //         this.uz = true
+        //     }else if (event.target.dataset.language == "en"){
+        //         store.state.lang = 2
+        //         this.uz = false
+        //         this.ru = false
+        //         this.en = true
+        //     }else if(event.target.dataset.language == "ru"){
+        //         store.state.lang = 3
+        //         this.ru = true
+        //         this.en = false
+        //         this.uz = false
+        //     }
+        //     console.log(store.state.lang);
+        //     console.log(this.env);
+        // }
+    },
+
+
+}
+</script>
+
+
+<template>
+    <header class="header h-[130px] w-[100%] fixed top-[0] z-[999]">
+        <div class="h-[100%] w-[90%] m-auto flex flex-col">
+            <div
+                class="lan flex h-[70px] w-[100%] items-center justify-between border-b-[1px] border-[solid] border-[#E6E6E6]">
+                <div class="h-[50px] w-[50px]  text-center">
+                    <img src="@/assets/icons/Final-011.svg" alt="">
+                </div>
+                <router-link class="text-[24px]" to="/">
+                    {{ this.text[this.env].header.header_title }}
+                </router-link>
+                <div>
+                    <span @click="changeLanguage" :class="{ languageActive: this.uz }" data-language="uz"
+                        class="text-[#AFAFAF] font-[600] cursor-pointer ml-[15px]">UZ</span>
+                    <span @click="changeLanguage" :class="{ languageActive: this.en }" data-language="en"
+                        class=" text-[#AFAFAF] font-[600] cursor-pointer ml-[15px]">ENG</span>
+                    <span @click="changeLanguage" :class="{ languageActive: this.ru }" data-language="ru"
+                        class="text-[#AFAFAF] font-[600] cursor-pointer ml-[15px]">RU</span>
+                </div>
+            </div>
+            <div class="flex h-[60px] w-[100%] items-center justify-center">
+
+                <router-link class="ml-[65px] link text-[#000]" to="/">Главная</router-link>
+                <router-link class="ml-[65px] link text-[#000]" to="/#About">О нас</router-link>
+                <router-link class="ml-[65px] link text-[#000]" to="/allproduct">Все товары</router-link>
+                <router-link class="ml-[65px] link text-[#000]" to="/education">Школа</router-link>
+                <router-link class="ml-[65px] link text-[#000]" to="/payment">Написать нам</router-link>
+            </div>
+
+
+
+        </div>
+    </header>
+    <!-- адаптив -->
+    <div>
+        <div class="header_mini h-[70px] w-[100%]  top-[0] z-[999]">
+            <div class="h-[100%] w-[90%] flex m-auto justify-between items-center">
+                <div class="h-[50px] w-[50px]  text-center">
+                    <img src="@/assets/icons/Final-011.svg" alt="">
+                </div>
+                <div class="flex items-center ">
+                    <span @click="changeLanguage" :class="{ languageActive: this.uz }" data-language="uz"
+                        class="text-[#AFAFAF] font-[600] text-[16px] cursor-pointer ml-[15px]">UZ</span>
+                    <span @click="changeLanguage" :class="{ languageActive: this.en }" data-language="en"
+                        class=" text-[#AFAFAF] font-[600] text-[16px] cursor-pointer ml-[15px]">ENG</span>
+                    <span @click="changeLanguage" :class="{ languageActive: this.ru }" data-language="ru"
+                        class="text-[#AFAFAF] font-[600] text-[16px] cursor-pointer ml-[15px]">RU</span>
+
+                    <div @click="BurgerFunc" :class="{ burgerLineActive: this.burger == true }"
+                        class="burgerBlock dusration-[10s] h-[25px] w-[30px] ml-[40px] flex flex-col justify-between">
+                        <div class="h-[1px] w-[100%] bg-[#000] "></div>
+                        <div class="h-[1px] w-[100%] bg-[#000] "></div>
+                        <div class="h-[1px] w-[100%] bg-[#000] "></div>
+                        <div class="h-[1px] w-[100%] bg-[#000] "></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div :class="{ burgerActive: this.burger == true }"
+            class="burgerM  absolute h-[500px] text-center right-[0px] bg-[#012465] top-[71px] w-[100%]  z-[999] ">
+
+            <div @click="BurgerFunc" class="flex flex-col h-[100%] w-[100%] items-center justify-center">
+                <router-link class=" z-[999] text-[20px] mt-[20px] text-[#fff]" to="/">Главная</router-link>
+                <router-link class=" z-[999] text-[20px] mt-[20px] text-[#fff]" to="/#About">О нас</router-link>
+                <router-link class=" z-[999] text-[20px] mt-[20px] text-[#fff]" to="/allproduct">Все товары</router-link>
+                <router-link class=" z-[999] text-[20px] mt-[20px] text-[#fff]" to="/education">Школа</router-link>
+                <router-link class=" z-[999] text-[20px] mt-[20px] text-[#fff]" to="/payment">Написать нам</router-link>
+            </div>
+        </div>
+    </div>
+    <!-- адаптив -->
+</template>
+
+
+<style>
+* {}
+
+.languageActive {
+    /* border-b-[1px] border-solid border-[#012465] */
+    /* text-[#012465]  */
+    border-bottom: 1px solid #012465;
+    color: #012465;
+}
+
+.header {
+    backdrop-filter: blur(50px);
+    /* box-shadow: 0px 2px 5px #dbdbdb; */
+}
+
+.header_mini {
+    backdrop-filter: blur(50px);
+}
+
+.burgerActive {
+    display: block !important;
+}
+
+.burgerM {
+    display: flex;
+    flex-direction: column;
+    display: none;
+}
+
+
+.burgerLineActive div {
+    opacity: 0;
+}
+
+.burgerLineActive {
+    background-image: url('../assets/icons/2931151_close_delete_exit_remove_x_icon.svg');
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: contain;
+}</style>
---
+++ src/components/Products.vue
@@ -0,0 +1,188 @@
+<script setup>
+</script>
+<script>
+import store from '../store';
+
+export default {
+    data() {
+        return {
+            panels: store.state.panels,
+            check: false,
+            filter: false,
+            filter2: false,
+        }
+    },
+    methods: {
+        checkFunc() {
+            this.check = !this.check
+        },
+        filterFunc(){
+            this.filter = !this.filter
+        },
+        filterFunc2(){
+            this.filter2 = !this.filter2
+        }
+    },
+}
+</script>
+
+<template>
+    <div class="productBlock min-h-[600px] w-[100%] bg-[#012465]">
+        <div class="h-[100%] w-[100%] flex">
+
+            <div class="filterProduct w-[385px] h-[max-content] sticky top-[130px] pt-[51px] pl-[80px]">
+                <ul class="">
+                    <li class="text-[#fff] mb-[20px]">Product type</li>
+
+                    <button @click="checkFunc" class="mb-[12px] flex items-center ">
+
+                        <div :class="{ checkActive: this.check == true }"
+                            class="h-[16px] w-[16px] border-[1px] border-solid border-[#4269E2] rounded-[2px]"></div>
+                        <p class="text-[#fff] ml-[12px]">Монокристаллические</p>
+                    </button>
+
+                    <button @click="checkFunc" class="mb-[12px] flex items-center ">
+
+                        <div :class="{ checkActive: this.check == true }"
+                            class="h-[16px] w-[16px] border-[1px] border-solid border-[#4269E2] rounded-[2px]"></div>
+                        <p class="text-[#fff] ml-[12px]">Высокая эффективность</p>
+                    </button>
+
+                    <button @click="checkFunc" class="mb-[12px] flex items-center ">
+
+                        <div :class="{ checkActive: this.check == true }"
+                            class="h-[16px] w-[16px] border-[1px] border-solid border-[#4269E2] rounded-[2px]"></div>
+                        <p class="text-[#fff] ml-[12px]">Эстетичный внешний вид</p>
+                    </button>
+
+                    <button @click="checkFunc" class="mb-[12px] flex items-center ">
+
+                        <div :class="{ checkActive: this.check == true }"
+                            class="h-[16px] w-[16px] border-[1px] border-solid border-[#4269E2] rounded-[2px]"></div>
+                        <p class="text-[#fff] ml-[12px]">Поликристаллические</p>
+                    </button>
+
+                    <button @click="checkFunc" class="mb-[12px] flex items-center ">
+
+                        <div :class="{ checkActive: this.check == true }"
+                            class="h-[16px] w-[16px] border-[1px] border-solid border-[#4269E2] rounded-[2px]"></div>
+                        <p class="text-[#fff] ml-[12px]">Тонкопленочные</p>
+                    </button>
+
+                    <button @click="checkFunc" class="mb-[12px] flex items-center ">
+
+                        <div :class="{ checkActive: this.check == true }"
+                            class="h-[16px] w-[16px] border-[1px] border-solid border-[#4269E2] rounded-[2px]"></div>
+                        <p class="text-[#fff] ml-[12px]">Малый вес</p>
+                    </button>
+
+
+                </ul>
+                <ul class="mt-[48px]">
+                    <li class="text-[#fff] mb-[20px]">Price</li>
+                    <button @click="checkFunc" class="mb-[12px] flex items-center ">
+
+                        <div :class="{ checkActive: this.check == true }"
+                            class="h-[16px] w-[16px] border-[1px] border-solid border-[#4269E2] rounded-[2px]"></div>
+                        <p class="text-[#fff] ml-[12px]">0 - 100</p>
+                    </button>
+
+                    <button @click="checkFunc" class="mb-[12px] flex items-center ">
+
+                        <div :class="{ checkActive: this.check == true }"
+                            class="h-[16px] w-[16px] border-[1px] border-solid border-[#4269E2] rounded-[2px]"></div>
+                        <p class="text-[#fff] ml-[12px]">100 - 250</p>
+                    </button>
+
+                    <button @click="checkFunc" class="mb-[12px] flex items-center ">
+
+                        <div :class="{ checkActive: this.check == true }"
+                            class="h-[16px] w-[16px] border-[1px] border-solid border-[#4269E2] rounded-[2px]"></div>
+                        <p class="text-[#fff] ml-[12px]">250+</p>
+                    </button>
+
+                </ul>
+            </div>
+<!-- при адаптиве -->
+
+<div  class="filter_block flex absolute mt-[20px] left-[50%] translate-x-[-50%]">
+                <div @click="filterFunc" class="filter__menu h-[56px] mr-[20px] w-[250px] relative bg-[#f9f9f9] flex items-center justify-center">
+                    Filters <img class="ml-[12px]" src="../assets/icons/Caret--down.png" alt="">
+                </div>
+                <div @click="filterFunc2" class="filter__menu  h-[56px] mr-[20px] w-[250px] relative bg-[#f9f9f9] flex items-center justify-center">
+                    Sorting <img class="ml-[12px]" src="../assets/icons/Caret--down.png" alt="">
+                </div>
+            </div>
+<!--  -->
+            <div :class="{filterActive: this.filter == true}" class="filter_m h-[100px] w-[250px] bg-[#f9f9f9] z-[99] absolute mt-[76px] ml-[114px]">
+                <ul class="filter-m__ul">
+                    <button @click="checkFunc" class="mb-[12px] flex items-center ">
+
+                        <div :class="{ checkActive: this.check == true }"
+                            class="h-[16px] w-[16px] border-[1px] border-solid border-[#4269E2] rounded-[2px]"></div>
+                        <p class="tex text-[#000] ml-[12px]">Монокри</p>
+                    </button>
+                    <button @click="checkFunc" class="mb-[12px] flex items-center ">
+
+                        <div :class="{ checkActive: this.check == true }"
+                            class="h-[16px] w-[16px] border-[1px] border-solid border-[#4269E2] rounded-[2px]"></div>
+                        <p class="tex text-[#000] ml-[12px]">Моно</p>
+                    </button>
+                </ul>
+            </div>
+
+            <div :class="{filterActive: this.filter2 == true}" class="filter_m filter2 h-[100px] w-[250px] bg-[#f9f9f9] z-[99] absolute mt-[76px] ml-[374px]">
+                <ul class="filter-m__ul">
+                    <button @click="checkFunc" class="mb-[12px] flex items-center ">
+
+                        <div :class="{ checkActive: this.check == true }"
+                            class="h-[16px] w-[16px] border-[1px] border-solid border-[#4269E2] rounded-[2px]"></div>
+                        <p class="text-[#000] ml-[12px]">0-100</p>
+                    </button>
+                    <button @click="checkFunc" class="mb-[12px] flex items-center ">
+
+                        <div :class="{ checkActive: this.check == true }"
+                            class="h-[16px] w-[16px] border-[1px] border-solid border-[#4269E2] rounded-[2px]"></div>
+                        <p class="text-[#000] ml-[12px]">100 - 250</p>
+                    </button>
+                </ul>
+            </div>
+            <!--  -->
+
+            
+
+
+        <!-- при адаптиве -->
+
+            <div class="productItems flex-1 pb-[120px] bg-[#fff] pl-[45px] flex flex-wrap">
+
+                <div v-for="i of this.panels" :key="i.id"
+                    class="productAll h-[462px] w-[305px] text-[#2A254B] mr-[20px] mt-[37px]">
+                    <img class="productImg h-[376px] w-[305px]" :src="i.img" alt="">
+                    <div class="flex justify-between mt-[24px] text-center h-[30px] items-center">
+                        <h2 class="text-[20px] text-[#2A254B]">{{ i.title }}</h2>
+                        <span class="text-[14px]">{{ i.wat }}W</span>
+                    </div>
+                    <p class="mt-[8px] text-[18px]">{{ i.price }}$</p>
+                </div>
+
+
+
+            </div>
+
+        </div>
+    </div>
+</template>
+
+<style>
+.checkActive {
+    background: #4269E2;
+    background-image: url('@/assets/icons/checkbox.png');
+    background-repeat: no-repeat;
+    background-size: contain;
+    background-position: center center;
+}
+.filterActive{
+    display: block;
+}
+</style>
